6066b1b91cf333a61b66bce8bc3b9045d9476a77
romanlazko <romanlazko@gmail.com>
2023-06-26 13:23:05 +0200

    Первый комит

---
+++ app/Bots/pozorbottestbot/Commands/CommandsList.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Bots\pozorbottestbot\Commands;
+
+use App\Bots\pozor_baraholka_bot\Commands\CommandsList as CommandsCommandsList;
+
+class CommandsList extends CommandsCommandsList
+{
+}
---
+++ app/Bots/pozorbottestbot/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Bots\pozorbottestbot;
+
+use App\Bots\pozor_baraholka_bot\Config as Pozor_baraholka_botConfig;
+
+class Config extends Pozor_baraholka_botConfig
+{
+}
