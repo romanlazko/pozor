f402fa3d6220264cd92d52c60b38fd04da432176
romanlazko <romanlazko@gmail.com>
2023-06-30 13:11:50 +0200

    Дизайнерские обновы

--- resources/views/user/baraholka/index.blade.php
+++ resources/views/user/baraholka/index.blade.php
@@ -1,36 +1,38 @@
 <x-guest-layout>
     <x-slot name="header">
         
-        <div class="flex items-center space-x-3">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{-- <div class="flex items-center space-x-3 w-full">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight w-full">
                 {{ __('Baraholka') }}
             </h2>
-            <div>
-                <form action="{{ route('baraholka') }}" class="form">
-                    <x-telegram::form.select name="type">
-                        <option value="">Type</option>
-                        @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('type')->pluck('type') as $type)
-                            <option value="{{ $type }}" @selected(old('type', request()->type) == $type)>{{ $type }}</option>
-                        @endforeach
-                    </x-telegram::form.select>
-                    <x-telegram::form.select name="category">
-                        <option value="">Category</option>
-                        @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('category')->pluck('category') as $category)
-                            <option value="{{ $category }}" @selected(old('category', request()->category) == $category)>{{ $category }}</option>
-                        @endforeach
-                    </x-telegram::form.select>
-                    <x-telegram::form.select name="city">
-                        <option value="">City</option>
-                        @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('city')->pluck('city') as $city)
-                            <option value="{{ $city }}" @selected(old('city', request()->city) == $city)>{{ $city }}</option>
-                        @endforeach
-                    </x-telegram::form.select>
-                    <x-telegram::buttons.primary>
-                        {{ __('Search') }}
-                    </x-telegram::buttons.primary>
-                </form>
-            </div>
-            
+        </div> --}}
+        <div class="w-full">
+            <form action="{{ route('baraholka') }}" class="grid sm:grid-cols-5 grid-cols-1 form space-y-2 items-center sm:space-x-3">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Baraholka') }}
+                </h2>
+                <x-telegram::form.select name="type">
+                    <option value="">Type</option>
+                    @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('type')->pluck('type') as $type)
+                        <option value="{{ $type }}" @selected(old('type', request()->type) == $type)>{{ ucfirst($type) }}</option>
+                    @endforeach
+                </x-telegram::form.select>
+                <x-telegram::form.select name="category">
+                    <option value="">Category</option>
+                    @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('category')->pluck('category') as $category)
+                        <option value="{{ $category }}" @selected(old('category', request()->category) == $category)>{{ ucfirst($category) }}</option>
+                    @endforeach
+                </x-telegram::form.select>
+                <x-telegram::form.select name="city">
+                    <option value="">City</option>
+                    @foreach (App\Bots\pozor_baraholka_bot\Models\BaraholkaAnnouncement::distinct('city')->pluck('city') as $city)
+                        <option value="{{ $city }}" @selected(old('city', request()->city) == $city)>{{ ucfirst($city) }}</option>
+                    @endforeach
+                </x-telegram::form.select>
+                <x-telegram::buttons.primary>
+                    {{ __('Search') }}
+                </x-telegram::buttons.primary>
+            </form>
         </div>
     </x-slot>
 
