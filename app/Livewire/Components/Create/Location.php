5ec7b893577e4145e662d92b8f69321db4b32ac8
romanlazko <romanlazko@gmail.com>
2024-03-09 13:37:11 +0100

    Проверка получения локации

--- app/Livewire/Components/Create/Location.php
+++ app/Livewire/Components/Create/Location.php
@@ -30,7 +30,7 @@ class Location extends Component
         $this->countries = Geo::getCountries();
         $this->country = $this->countries->first()?->country;
         
-        $this->location = Geo::findName(CurrentLocation::get()?->regionName)?->toArray();
+        $this->location = Geo::findName((CurrentLocation::get() ?: null)?->regionName)?->toArray();
         $this->search = $this->location['name'] ?? null;
     }
 
