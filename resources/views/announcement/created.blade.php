18bbe97df28938236f3707e9cd66b1d3e85edd9e
romanlazko <romanlazko@gmail.com>
2024-05-17 01:29:13 +0200

    Попытка закрыть тприложение после создания объявления

--- resources/views/announcement/created.blade.php
+++ resources/views/announcement/created.blade.php
@@ -1,5 +1,10 @@
-<x-body-layout>
-    <div class="bg-white p-4 max-w-md m-auto my-2 rounded-lg space-y-4 shadow-xl h-full">
+{{-- <x-body-layout> --}}
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <script>
+        window.Telegram.WebApp.close();
+    </script>
+    
+    {{-- <div class="bg-white p-4 max-w-md m-auto my-2 rounded-lg space-y-4 shadow-xl h-full">
         <div class="text-center px-4 py-2 rounded-md space-y-6">
             <div class="flex items-center justify-center h-10 w-10 min-w-10 min-h-10 bg-green-500 rounded-full mx-auto">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -8,5 +13,5 @@
             </div>
             <span class="block text-lg font-semibold">Ваше объявление успешно создано!</span>
         </div>
-    </div>
-</x-body-layout>
+    </div> --}}
+{{-- </x-body-layout> --}}
