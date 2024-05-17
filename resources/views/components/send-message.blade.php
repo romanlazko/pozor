ad24f5808f359ecc005a811e4f6e2da3dac7e37a
romanlazko <romanlazko@gmail.com>
2024-05-17 17:48:52 +0200

    Исправление бага

--- resources/views/components/send-message.blade.php
+++ resources/views/components/send-message.blade.php
@@ -3,11 +3,11 @@
 <x-white-block>
     <div class="w-full space-y-4">
         <div class="flex space-x-4 items-center">
-            <img src="{{ $announcement->user->getFirstMediaUrl('avatar', 'thumb') }}" alt="" class="rounded-full w-14 h-14 lg:w-16 lg:h-16 aspect-square">
+            <img src="{{ $announcement->user?->getFirstMediaUrl('avatar', 'thumb') }}" alt="" class="rounded-full w-14 h-14 lg:w-16 lg:h-16 aspect-square">
             <div class="w-full">
-                <span class="block font-bold">{{ $announcement->user->name }}</span>
-                <a class="block text-gray-700 hover:underline cursor-pointer">{{ $announcement->user->email }}</a>
-                <span class="block text-gray-500 text-xs">{{ $announcement->user->created_at->diffForHumans() }}</span>
+                <span class="block font-bold">{{ $announcement->user?->name }}</span>
+                <a class="block text-gray-700 hover:underline cursor-pointer">{{ $announcement->user?->email }}</a>
+                <span class="block text-gray-500 text-xs">{{ $announcement->user?->created_at->diffForHumans() }}</span>
             </div>
         </div>
 
