1bbff182d1d36e2e4199ec32b550cc8083151dac
romanlazko <romanlazko@gmail.com>
2024-06-05 18:54:06 +0200

    Убрал поле name там где оно не нужно и вызывает ошбику

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -32,8 +32,8 @@ class AnnouncementController extends Controller
         $announcements = Announcement::with([
                 'media',
                 'features:id,announcement_id,attribute_id,attribute_option_id,translated_value',
-                'features.attribute:id,name,alterlabels,order_number,searchable,is_feature,altersyffixes,create_type',
-                'features.attribute_option:id,name,alternames',
+                'features.attribute:id,alterlabels,order_number,searchable,is_feature,altersyffixes,create_type',
+                'features.attribute_option:id,alternames',
                 'user', 
                 'userVotes'
             ])
