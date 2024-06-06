db1baabd8ef5fb0410f5fafdbb47f95668bb48a2
romanlazko <romanlazko@gmail.com>
2024-06-06 11:12:49 +0200

    оставил только recaptcha

--- routes/auth.php
+++ routes/auth.php
@@ -21,7 +21,7 @@ Route::middleware(['guest'])->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->middleware(['honey', 'honey-recaptcha']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->middleware(['honey-recaptcha']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
