11737f4d84bc7bdb6944f52171a982815189ca0b
romanlazko <romanlazko@gmail.com>
2024-06-17 19:23:12 +0200

    Проверка работы работы

--- app/Jobs/PublishAnnouncementJob.php
+++ app/Jobs/PublishAnnouncementJob.php
@@ -28,7 +28,9 @@ class PublishAnnouncementJob
 
     public function handle(): void
     {
-        $announcement = Announcement::where('id', $this->announcement_id)->first();
+        $announcement = Announcement::where('id', $this->announcement_id)->with(['channels' => function ($query) {
+            return $query->where('status', '!=', Status::published);
+        }])->first();
 
         dd($announcement->channels);
 
