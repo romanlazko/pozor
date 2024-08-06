e099a304ffdc490d0ddb8590813a532b9ddff8be
romanlazko <romanlazko@gmail.com>
2024-08-07 01:10:02 +0200

    Изменил вид публикации в телеграм

--- resources/views/telegram/announcement/show.blade.php
+++ resources/views/telegram/announcement/show.blade.php
@@ -1,2 +1,10 @@
-<b>#{{ $announcement->getFeatureByName('type')?->value }}</b>
+<b>#{{ ($announcement->getFeatureByName('real_estate_type') ?? $announcement->getFeatureByName('type'))?->value }}</b>
+
 <b>{{ $announcement->getFeatureByName('title')?->value }}</b>
+
+<a href="{{ route('announcement.show', $announcement) }}">Посмотреть объявление</a>
+
+@foreach ($announcement->categories as $category)
+    <b>#{{ $category->name }}</b>
+@endforeach
+
