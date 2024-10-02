c3fb872861aae99fe21baa2ee6ba40ddcfe1b599
romanlazko <romanlazko@gmail.com>
2024-10-02 14:35:38 +0200

    Проверка работы передачи айди объявления

--- app/Livewire/SendMessage.php
+++ app/Livewire/SendMessage.php
@@ -24,8 +24,8 @@ class SendMessage extends Component implements HasForms, HasActions
     public function mount($announcement_id)
     {
         dump($announcement_id, $this->announcement_id);
-        
-        $this->announcement = Announcement::findOrFail($this->announcement_id);
+
+        $this->announcement = Announcement::findOrFail(21);
     }
 
     public function sendMessage()
