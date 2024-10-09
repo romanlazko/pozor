935553fb0d75d477453445cc266fbb7cc284206d
romanlazko <romanlazko@gmail.com>
2024-10-09 16:46:34 +0200

    Изменил размер кнопок для слайдера

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -127,14 +127,14 @@
                 <div class="absolute inset-0 flex justify-between items-center z-10">
                     <button @click.prevent="prevSlide()" @dblclick.prevent class="h-full p-1 lg:px-3">
                         <div class="bg-gray-800 bg-opacity-50 lg:hover:bg-opacity-90 text-white p-2 rounded-full opacity-30 hover:opacity-100 lg:opacity-100 ">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 lg:h-6 w-3 lg:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
                             </svg>
                         </div>
                     </button>
                     <button @click.prevent="nextSlide()" @dblclick.prevent class="h-full p-1 lg:px-3">
                         <div class="bg-gray-800 bg-opacity-50 lg:hover:bg-opacity-90 text-white p-2 rounded-full opacity-30 hover:opacity-100 lg:opacity-100 ">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 lg:h-6 w-3 lg:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                             </svg>
                         </div>
